ef38f7d1f8d82e06a1cf79b6bed77378a8049f7e
Shivani Kumari <67224174+Shivani-Kumari-1@users.noreply.github.com>
2020-06-25 12:36:50 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,39 @@
+/*
+
+1
+21
+321
+4321
+54321
+4321
+321
+21
+1
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter number = ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=i; j>=1; j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    for(i=n-1; i>=1; i--)
+    {
+        for(j=i; j>=1; j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
